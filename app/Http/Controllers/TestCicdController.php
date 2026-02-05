db2680e4d2ccb7728eeb899d738518b497c9ba0d
thanhnv <thanhnv@sabitech.vn>
2026-02-05 15:58:22 +0700

    feat: add  N+1

--- app/Http/Controllers/TestCicdController.php
+++ app/Http/Controllers/TestCicdController.php
@@ -1,12 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\User;
 use Illuminate\Http\Request;
 
-class TestCicdController extends Controller {
-
-public function index()
+class TestCicdController extends Controller
 {
+    public function index()
+    {
+        $users = User::all(); // 1 query
 
+        foreach ($users as $user) {
+            $posts = $user->posts()->get();
+        }
+
+        return response()->json(['ok' => true]);
     }
 }
