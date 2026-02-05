90bcd68c4a737bfed295dc2345e1e4f030c9c705
thanhnv <thanhnv@sabitech.vn>
2026-02-05 16:16:28 +0700

    fix n+1

--- app/Http/Controllers/TestCicdController.php
+++ app/Http/Controllers/TestCicdController.php
@@ -11,7 +11,7 @@ class TestCicdController extends Controller
         $users = User::all();
 
         foreach ($users as $user) {
-            $user->fakePosts()->count();
+            $user->fakePosts();
         }
 
         return response()->json(['ok' => true]);
